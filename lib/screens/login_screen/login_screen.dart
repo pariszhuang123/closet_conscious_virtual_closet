4d1dde2ae4be93f0e51bc865595b3007fc8aeecb
Paris Zhuang <pariszhuang@gmail.com>
2024-03-12 23:15:40 +1300

    Update the buttons and only keep sign in screen

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -2,85 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:closet_conscious/generated/l10n.dart';
 import 'package:closet_conscious/screens/login_screen/login/login_with_google.dart'; // Adjust the import path as necessary
 import 'package:closet_conscious/screens/login_screen/login/login_with_apple.dart'; // Adjust the import path as necessary
-import 'package:closet_conscious/screens/login_screen/signup/signup_with_google.dart'; // Adjust the import path as necessary
-import 'package:closet_conscious/screens/login_screen/signup/signup_with_apple.dart'; // Adjust the import path as necessary
-
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Now using localization
-    final S loc = S.of(context); // Get localization instance
+    final S loc = S.of(context);
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  // Your logo here
-                  // You can replace this with an Image widget if you have a logo image file
-                  CircleAvatar(
-                    backgroundColor: Colors.transparent, // Assuming logo background is transparent
-                    radius: 48.0,
-                    child: Text(
-                      loc.appName, // Replace 'Closet Conscious' with a localized string
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20), // Provides space between logo and tab bar
-                ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Your logo here
+              Image.network('https://vrhytwexijijwhlicqfw.supabase.co/storage/v1/object/public/closet-conscious-assets/Closet%20Conscious%20Logo.svg?t=2024-03-12T09%3A16%3A41.280Z'),
+              SizedBox(height: 20),
+              Text(
+                loc.appName,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            TabBar(
-              tabs: [
-                Tab(text: loc.signIn),
-                Tab(text: loc.signUp),
-              ],
-            ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  // Sign In Tab Content
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(loc.loginSocialAccount),
-                        const SizedBox(height: 20),
-                        LoginWithGoogle(), // Use the LoginWithGoogle widget
-                        LoginWithApple(), // Use the LoginWithApple widget
-                      ],
-                    ),
-                  ),
-                  // Sign Up Tab Content
-                  // Sign Up Tab Content
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(S.of(context).SignupSocialAccount),
-                        const SizedBox(height: 20),
-                        const SignupWithGoogle(), // Use the SignupWithGoogle widget
-                        const SignupWithApple(), // Use the SignupWithApple widget
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Text(loc.loginSocialAccount),
+              SizedBox(height: 20),
+              LoginWithGoogle(), // Display Google Sign-In button
+              LoginWithApple(), // Display Apple Sign-In button
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
