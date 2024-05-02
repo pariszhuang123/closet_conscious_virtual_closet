086e6edd2c271e05bcbac4d255308764b01de1e7
pariszhuang <pariszhuang@gmail.com>
2024-05-02 20:17:32 +1200

    Remove the signIn Process from supabase_service.dart as I am not allowing users to sign in with other methods

--- lib/core/supabase_service.dart
+++ lib/core/supabase_service.dart
@@ -1,38 +1,12 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../config_reader.dart';
-import 'package:logger/logger.dart';
 
 class SupabaseService {
-  static final Logger _logger = Logger();
 
   static Future<void> initialize() async {
     await Supabase.initialize(
         url: ConfigReader.getSupabaseUrl(),
         anonKey: ConfigReader.getSupabaseAnonKey()
     );
-  }
-
-  Future<void> signIn(String email, String password) async {
-    try {
-      final response = await Supabase.instance.client.auth.signIn(
-        email: email,
-        password: password,
-      );
-      if (response.error != null) {
-        throw Exception('Sign-in failed: ${response.error!.message}');
-      }
-    } catch (e) {
-      _logger.e('Sign in error: $e');
-      rethrow; // Only rethrow if you want calling function to handle it as well
-    }
-  }
-
-  Future<void> signOut() async {
-    try {
-      await Supabase.instance.client.auth.signOut();
-    } catch (e) {
-      _logger.e('Sign out error: $e');
-      rethrow; // Only rethrow if you want calling function to handle it as well
-    }
   }
 }
--- lib/main_common.dart
+++ lib/main_common.dart
@@ -24,7 +24,7 @@ Future<void> mainCommon(String environment) async {
 class MyApp extends StatelessWidget {
   final String environment;
 
-  MyApp({required this.environment});
+  const MyApp({Key? key, required this.environment}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
