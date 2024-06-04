622dbdaca49d2342176196915f8f5a0454bb06b5
pariszhuang <pariszhuang@gmail.com>
2024-06-04 21:03:06 +1200

    My sync user_profile didn't work, now amended

---
+++ supabase/migrations/20240604090055_create_sync_user_profile_function.sql
@@ -0,0 +1,17 @@
+-- inserts a row into public.profiles
+create function public.sync_user_profile()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.user_profiles (id, email, name, role, created_at, updated_at)
+  values (new.id, new.raw_user_meta_data ->> 'name', new.email, 'authenticated', now(), now());
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger create_user_profile
+  after insert on auth.users
+  for each row execute procedure public.user_profiles();
