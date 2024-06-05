37beb20c47f892935bbb4e9d1a0cb49ef1a30460
pariszhuang <pariszhuang@gmail.com>
2024-06-05 19:41:50 +1200

    Try to get user profile function and trigger working.

---
+++ supabase/migrations/20240605074055_updated_sync_user_profile_function.sql
@@ -0,0 +1,18 @@
+-- inserts a row into public.user_profiles
+CREATE OR REPLACE FUNCTION public.sync_user_profile()
+RETURNS TRIGGER AS $$
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = 'public, auth'
+AS $$
+BEGIN
+  INSERT INTO public.user_profiles (user_id, name, email, role, created_at, updated_at)
+  VALUES (NEW.id, NEW.raw_user_meta_data ->> 'name', NEW.email, 'authenticated', now(), now());
+  RETURN NEW;
+END;
+$$;
+
+-- trigger the function every time a user is created
+CREATE TRIGGER create_user_profile
+AFTER INSERT ON auth.users
+FOR EACH ROW EXECUTE FUNCTION public.sync_user_profile();
