d742f3f0155e290eb9240c98ea1cd64c0a2aea5a
pariszhuang <pariszhuang@gmail.com>
2024-06-05 21:54:41 +1200

    Try to first drop the trigger and function

--- supabase/migrations/20240605074055_updated_sync_user_profile_function.sql
+++ supabase/migrations/20240605074055_updated_sync_user_profile_function.sql
@@ -1,19 +1,5 @@
--- Inserts a row into public.user_profiles
-CREATE OR REPLACE FUNCTION public.sync_user_profile()
-RETURNS TRIGGER AS $$
-LANGUAGE plpgsql
-SECURITY DEFINER
-SET search_path = 'public, auth'
-AS $$
-BEGIN
-  INSERT INTO public.user_profiles (user_id, name, email, role, created_at, updated_at)
-  VALUES (NEW.id::uuid, NEW.raw_user_meta_data ->> 'name', NEW.email::text, 'authenticated', now(), now());
-  RETURN NEW;
-END;
-$$;
+-- Step 1: Drop the trigger
+DROP TRIGGER IF EXISTS create_user_profile ON auth.users;
 
--- trigger the function every time a user is created
-CREATE TRIGGER create_user_profile
-AFTER INSERT ON auth.users
-FOR EACH ROW EXECUTE FUNCTION public.sync_user_profile();
-
+-- Step 2: Drop the function
+DROP FUNCTION IF EXISTS sync_user_profile();
