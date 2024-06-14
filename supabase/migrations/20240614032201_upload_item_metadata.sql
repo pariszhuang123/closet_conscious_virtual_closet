79b3b9596199b6f239e16f8044c780af5cc477b6
pariszhuang <pariszhuang@gmail.com>
2024-06-14 15:24:23 +1200

    upload RPC upload_item_metadata

---
+++ supabase/migrations/20240614032201_upload_item_metadata.sql
@@ -0,0 +1,25 @@
+-- Your SQL function definition
+create or replace function upload_item_metadata(
+  _item_id uuid,
+  _item_type text,
+  _image_url text,
+  _name text,
+  _amount_spent numeric,
+  _occasion text,
+  _season text,
+  _colour text,
+  _colour_variations text,
+  _clothing_type text,
+  _clothing_layer text
+)
+returns void as $$
+begin
+  -- Insert into items table
+  insert into items (item_id, item_type, image_url, name, amount_spent, occasion, season, colour, colour_variations)
+  values (_item_id, _item_type, _image_url, _name, _amount_spent, _occasion, _season, _colour, _colour_variations);
+
+  -- Insert into items_clothing_basic table
+  insert into items_clothing_basic (item_id, clothing_type, clothing_layer)
+  values (_item_id, _clothing_type, _clothing_layer);
+end;
+$$ language plpgsql;
