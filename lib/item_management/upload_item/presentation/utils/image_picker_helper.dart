8279ecbc19203b6283237e4e8e4b1004ea766e46
pariszhuang <pariszhuang@gmail.com>
2024-06-15 23:47:51 +1200

    Refactor and clean up unnecessary files

--- lib/item_management/upload_item/presentation/utils/image_picker_helper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-
-class ImagePickerHelper {
-  final ImagePicker _picker = ImagePicker();
-
-  Future<XFile?> takePhoto() async {
-    return await _picker.pickImage(source: ImageSource.camera);
-  }
-}
