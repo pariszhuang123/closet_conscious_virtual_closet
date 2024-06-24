3b554a8d1ddd29ed471f37739ff6aa89f38a9c4b
pariszhuang <pariszhuang@gmail.com>
2024-06-24 23:43:16 +1200

    Remove overflow and centralized button and add cross

--- lib/core/widgets/filter_premium_bottom_sheet.dart
+++ lib/core/widgets/filter_premium_bottom_sheet.dart
@@ -8,51 +8,57 @@ class PremiumFilterBottomSheet extends StatelessWidget {
 
   const PremiumFilterBottomSheet({super.key, required this.isFromMyCloset});
 
+
   @override
   Widget build(BuildContext context) {
-    // Determine colors based on originating page
-    ColorScheme colorScheme = isFromMyCloset ? myClosetTheme.colorScheme : myOutfitTheme.colorScheme;
+    // Determine the theme and colors based on originating page
+    ThemeData theme = isFromMyCloset ? myClosetTheme : myOutfitTheme;
+    ColorScheme colorScheme = theme.colorScheme;
 
     return Container(
       color: colorScheme.background,
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                S.of(context).filterSearchPremiumFeature,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: colorScheme.onBackground),
-              ),
-              IconButton(
-                icon: Icon(Icons.close, color: colorScheme.onBackground),
-                onPressed: () {
-                  Navigator.pop(context); // Close the bottom sheet
-                },
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          Text(
-            S.of(context).quicklyFindItems,
-            style: TextStyle(fontSize: 16, color: colorScheme.onBackground),
-          ),
-          const SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () {
-              // Handle user interest in the premium feature
-              Navigator.pop(context); // Close the bottom sheet
-              // You can add further logic here to navigate to the purchase page or show more details
-            },
-            style: ElevatedButton.styleFrom(
-              foregroundColor: colorScheme.onPrimary, backgroundColor: colorScheme.primary,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  S.of(context).filterSearchPremiumFeature,
+                  style: theme.textTheme.titleMedium, // Apply titleMedium style
+                ),
+                IconButton(
+                  icon: Icon(Icons.close, color: colorScheme.onBackground),
+                  onPressed: () {
+                    Navigator.pop(context); // Close the bottom sheet
+                  },
+                ),
+              ],
             ),
-            child: Text(S.of(context).interested),
-          ),
-        ],
+            const SizedBox(height: 8.0),
+            Text(
+              S.of(context).quicklyFindItems,
+              style: theme.textTheme.bodyMedium, // Apply bodyMedium style
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle user interest in the premium feature
+                  Navigator.pop(context); // Close the bottom sheet
+                  // You can add further logic here to navigate to the purchase page or show more details
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: colorScheme.onPrimary, backgroundColor: colorScheme.primary,
+                ),
+                child: Text(S.of(context).interested, style: theme.textTheme.labelLarge), // Apply labelLarge text style
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
