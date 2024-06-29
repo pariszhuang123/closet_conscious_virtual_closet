7262e9e94d67d96e388aa349d67cae5eaee3193a
pariszhuang <pariszhuang@gmail.com>
2024-06-29 16:22:37 +1200

    Warnings address filter test

---
+++ supabase/migrations/20240629042004_revised_filter_user_low_freq_function.sql
@@ -0,0 +1,11 @@
+-- Increment filter requests
+CREATE OR REPLACE FUNCTION increment_filter_request(p_user_id uuid)
+    RETURNS void AS $$
+    SET search_path = public.user_low_freq_stats as $$
+
+BEGIN
+    UPDATE public.user_low_freq_stats
+    SET filter_request = filter_request + 1
+    WHERE user_id = p_user_id;
+END;
+$$ LANGUAGE plpgsql;
