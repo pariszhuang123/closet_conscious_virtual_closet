802e6c6f3a63192a77bdfceb81d30dd63dc479e3
pariszhuang <pariszhuang@gmail.com>
2024-07-03 18:24:58 +1200

    Revised Increment functions

--- supabase/migrations/20240703062414_revised_increment_functions.sql
+++ supabase/migrations/20240703062414_revised_increment_functions.sql
@@ -1,13 +1,12 @@
 -- Increment filter requests
 CREATE OR REPLACE FUNCTION increment_filter_request()
-    RETURNS void
-    SET search_path = ''
-
+RETURNS void
+SET search_path = ''
 AS $$
 DECLARE
   current_user_id uuid := auth.uid();
 BEGIN
-    UPDATE user_low_freq_stats
+    UPDATE public.user_low_freq_stats
     SET filter_request = filter_request + 1
     WHERE user_id = current_user_id;
 END;
@@ -15,11 +14,13 @@ $$ LANGUAGE plpgsql;
 
 -- Increment multi-closet requests
 CREATE OR REPLACE FUNCTION increment_multi_closet_request()
-RETURNS void AS $$
+RETURNS void
+SET search_path = ''
+AS $$
 DECLARE
   current_user_id uuid := auth.uid();
 BEGIN
-    UPDATE user_low_freq_stats
+    UPDATE public.user_low_freq_stats
     SET multi_closet_request = multi_closet_request + 1
     WHERE user_id = current_user_id;
 END;
@@ -27,11 +28,13 @@ $$ LANGUAGE plpgsql;
 
 -- Increment analytics request
 CREATE OR REPLACE FUNCTION increment_analytics_request()
-RETURNS void AS $$
+RETURNS void
+SET search_path = ''
+AS $$
 DECLARE
   current_user_id uuid := auth.uid();
 BEGIN
-    UPDATE user_low_freq_stats
+    UPDATE public.user_low_freq_stats
     SET analytics_request = analytics_request + 1
     WHERE user_id = current_user_id;
 END;
@@ -39,11 +42,13 @@ $$ LANGUAGE plpgsql;
 
 -- Increment items swapped
 CREATE OR REPLACE FUNCTION increment_items_swapped()
-RETURNS void AS $$
+RETURNS void
+SET search_path = ''
+AS $$
 DECLARE
   current_user_id uuid := auth.uid();
 BEGIN
-    UPDATE user_low_freq_stats
+    UPDATE public.user_low_freq_stats
     SET items_swapped = items_swapped + 1
     WHERE user_id = current_user_id;
 END;
@@ -51,11 +56,13 @@ $$ LANGUAGE plpgsql;
 
 -- Increment items sold
 CREATE OR REPLACE FUNCTION increment_items_sold()
-RETURNS void AS $$
+RETURNS void
+SET search_path = ''
+AS $$
 DECLARE
   current_user_id uuid := auth.uid();
 BEGIN
-    UPDATE user_low_freq_stats
+    UPDATE public.user_low_freq_stats
     SET items_sold = items_sold + 1
     WHERE user_id = current_user_id;
 END;
@@ -63,11 +70,13 @@ $$ LANGUAGE plpgsql;
 
 -- Increment items gifted
 CREATE OR REPLACE FUNCTION increment_items_gifted()
-RETURNS void AS $$
+RETURNS void
+SET search_path = ''
+AS $$
 DECLARE
   current_user_id uuid := auth.uid();
 BEGIN
-    UPDATE user_low_freq_stats
+    UPDATE public.user_low_freq_stats
     SET items_gifted = items_gifted + 1
     WHERE user_id = current_user_id;
 END;
