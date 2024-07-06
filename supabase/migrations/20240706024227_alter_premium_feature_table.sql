087a314b861cc0ba1eb66d1630d6162c20bf5d0e
pariszhuang <pariszhuang@gmail.com>
2024-07-06 14:43:11 +1200

    Update premium service table

---
+++ supabase/migrations/20240706024227_alter_premium_feature_table.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+-- Drop primary key constraint
+ALTER TABLE premium_service DROP CONSTRAINT premium_service_pkey;
+
+-- Drop columns
+ALTER TABLE premium_service
+DROP COLUMN service_type,
+DROP COLUMN sub_service,
+DROP COLUMN activation_date,
+DROP COLUMN status;
+
+-- Update columns to be NOT NULL
+ALTER TABLE premium_service
+ALTER COLUMN created_at SET NOT NULL,
+ALTER COLUMN updated_at SET NOT NULL;
+
+-- Add new columns with default values
+ALTER TABLE premium_service
+ADD COLUMN one_off_features jsonb NOT NULL DEFAULT '{}',
+ADD COLUMN usage_feature jsonb NOT NULL DEFAULT '{}',
+ADD COLUMN challenge_feature jsonb NOT NULL DEFAULT '{}';
+
+COMMIT;
