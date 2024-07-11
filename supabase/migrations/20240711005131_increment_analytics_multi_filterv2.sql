54f8f1c749153bdd94d289704b7c11280dd7f35a
pariszhuang <pariszhuang@gmail.com>
2024-07-11 12:52:28 +1200

    Update public schema reference for increment tables

---
+++ supabase/migrations/20240711005131_increment_analytics_multi_filterv2.sql
@@ -0,0 +1,93 @@
+-- drop functions
+drop function if exists increment_filter_request();
+
+drop function if exists increment_analytics_request();
+
+drop function if exists increment_multi_closet_request();
+
+-- Increment filter requests
+CREATE OR REPLACE FUNCTION increment_filter_request()
+RETURNS json
+SET search_path = ''
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    current_user_id uuid := auth.uid();
+    result json;
+BEGIN
+    -- Update the filter request count for the current user
+    UPDATE public.user_low_freq_stats
+    SET filter_request = filter_request + 1
+    WHERE user_id = current_user_id;
+
+    -- Check if the update was successful
+    IF FOUND THEN
+        result := json_build_object('status', 'success', 'message', 'Recorded filtering request');
+    ELSE
+        result := json_build_object('status', 'failure', 'message', 'Cannot record filtering request');
+    END IF;
+
+    RETURN result;
+EXCEPTION
+    WHEN others THEN
+        RETURN json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+END;
+$$;
+
+
+-- Increment multi-closet requests
+CREATE OR REPLACE FUNCTION increment_multi_closet_request()
+RETURNS json
+SET search_path = ''
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    current_user_id uuid := auth.uid();
+    result json;
+BEGIN
+    UPDATE public.user_low_freq_stats
+    SET multi_closet_request = multi_closet_request + 1
+    WHERE user_id = current_user_id;
+
+    -- Check if the update was successful
+    IF FOUND THEN
+        result := json_build_object('status', 'success', 'message', 'Recorded multi-closet request');
+    ELSE
+        result := json_build_object('status', 'failure', 'message', 'Cannot record multi-closet request');
+    END IF;
+
+    RETURN result;
+EXCEPTION
+    WHEN others THEN
+        RETURN json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+END;
+$$;
+
+
+-- Increment analytics requests
+CREATE OR REPLACE FUNCTION increment_analytics_request()
+RETURNS json
+SET search_path = ''
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    current_user_id uuid := auth.uid();
+    result json;
+BEGIN
+    UPDATE public.user_low_freq_stats
+    SET analytics_request = analytics_request + 1
+    WHERE user_id = current_user_id;
+
+    -- Check if the update was successful
+    IF FOUND THEN
+        result := json_build_object('status', 'success', 'message', 'Recorded analytics request');
+    ELSE
+        result := json_build_object('status', 'failure', 'message', 'Cannot record analytics request');
+    END IF;
+
+    RETURN result;
+EXCEPTION
+    WHEN others THEN
+        RETURN json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+END;
+$$;
