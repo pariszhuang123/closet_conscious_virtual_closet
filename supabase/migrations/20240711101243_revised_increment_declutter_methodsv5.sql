49028c155bc5a558ac0ebb456fd098295546f972
pariszhuang <pariszhuang@gmail.com>
2024-07-11 22:26:35 +1200

    Revised increment declutter

---
+++ supabase/migrations/20240711101243_revised_increment_declutter_methodsv5.sql
@@ -0,0 +1,254 @@
+-- Function for increment_items_thrown
+create or replace function public.increment_items_thrown(current_item_id uuid)
+returns json
+SET search_path = ''
+language plpgsql
+as $$
+declare
+  current_user_id uuid := auth.uid();
+  result json;
+begin
+    -- Update item status
+    update public.items
+    set
+        sub_status = 'throw_away',
+        status = 'inactive',
+        updated_at = now()
+    where
+        current_owner_id = current_user_id and
+        item_id = current_item_id;
+
+    -- Update user high frequency stats if item was uploaded but never worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit = 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user high frequency stats if item was uploaded and worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1,
+        initial_upload_items_worn_count = initial_upload_items_worn_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit > 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+        result := json_build_object('status', 'success', 'message', 'Item successfully thrown away');
+    else
+        result := json_build_object('status', 'failure', 'message', 'Item does not exist or cannot be thrown away');
+    end if;
+
+    return result;
+
+exception
+    when others then
+        return json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+end;
+$$;
+
+-- Function for increment_items_sold
+create or replace function public.increment_items_sold(current_item_id uuid)
+returns json
+SET search_path = ''
+language plpgsql
+as $$
+declare
+  current_user_id uuid := auth.uid();
+  result json;
+begin
+    -- Update item status
+    update public.items
+    set
+        sub_status = 'sold',
+        status = 'inactive',
+        updated_at = now()
+    where
+        current_owner_id = current_user_id and
+        item_id = current_item_id;
+
+    -- Update user high frequency stats if item was uploaded but never worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit = 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user high frequency stats if item was uploaded and worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1,
+        initial_upload_items_worn_count = initial_upload_items_worn_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit > 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user low frequency stats
+    update public.user_low_freq_stats
+    set
+        items_sold = items_sold + 1,
+        updated_at = now()
+    where
+        user_id = current_user_id;
+
+        result := json_build_object('status', 'success', 'message', 'Item successfully sold');
+    else
+        result := json_build_object('status', 'failure', 'message', 'Item does not exist or cannot be sold');
+    end if;
+
+    return result;
+
+exception
+    when others then
+        return json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+end;
+$$;
+
+-- Function for increment_items_gifted
+create or replace function public.increment_items_gifted(current_item_id uuid)
+returns json
+SET search_path = ''
+language plpgsql
+as $$
+declare
+  current_user_id uuid := auth.uid();
+  result json;
+begin
+    -- Update item status
+    update public.items
+    set
+        sub_status = 'gift',
+        status = 'inactive',
+        updated_at = now()
+    where
+        current_owner_id = current_user_id and
+        item_id = current_item_id;
+
+    -- Update user high frequency stats if item was uploaded but never worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit = 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user high frequency stats if item was uploaded and worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1,
+        initial_upload_items_worn_count = initial_upload_items_worn_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit > 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user low frequency stats
+    update public.user_low_freq_stats
+    set
+        items_gifted = items_gifted + 1,
+        updated_at = now()
+    where
+        user_id = current_user_id;
+
+        result := json_build_object('status', 'success', 'message', 'Item successfully gifted');
+    else
+        result := json_build_object('status', 'failure', 'message', 'Item does not exist or cannot be gifted');
+    end if;
+
+    return result;
+exception
+    when others then
+        return json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+end;
+$$;
+
+-- Function for items_swapped
+create or replace function public.increment_items_swapped(current_item_id uuid)
+returns json
+SET search_path = ''
+language plpgsql
+as $$
+declare
+  current_user_id uuid := auth.uid();
+  result json;
+begin
+    -- Update item status
+    update public.items
+    set
+        sub_status = 'swap',
+        status = 'inactive',
+        updated_at = now()
+    where
+        current_owner_id = current_user_id and
+        item_id = current_item_id;
+
+    -- Update user high frequency stats if item was uploaded but never worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit = 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user high frequency stats if item was uploaded and worn in an outfit
+    update public.user_high_freq_stats
+    set
+        initial_upload_item_count = initial_upload_item_count - 1,
+        initial_upload_items_worn_count = initial_upload_items_worn_count - 1
+    where
+        user_id = current_user_id and
+        exists (
+            select 1
+            from public.items
+            where closet_upload = true and worn_in_outfit > 0 and current_owner_id = current_user_id and item_id = current_item_id
+        );
+
+    -- Update user low frequency stats
+    update public.user_low_freq_stats
+    set
+        items_swapped = items_swapped + 1,
+        updated_at = now()
+    where
+        user_id = current_user_id;
+
+        result := json_build_object('status', 'success', 'message', 'Item successfully swapped');
+    else
+        result := json_build_object('status', 'failure', 'message', 'Item does not exist or cannot be swapped');
+    end if;
+
+    return result;
+
+exception
+    when others then
+        return json_build_object('status', 'error', 'message', format('An error occurred: %s', SQLERRM));
+end;
+$$;
