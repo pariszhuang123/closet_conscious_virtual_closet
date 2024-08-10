a4b37b58876ff9d10677ddcee0760847078b238b
pariszhuang <pariszhuang@gmail.com>
2024-08-10 17:53:01 +1200

    Create new function to extract outfit items

---
+++ supabase/migrations/20240810054314_get_outfit_items.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION public.get_outfit_items(outfit_id UUID)
+SET search_path = ''
+RETURNS TABLE(item_id UUID, image_url text, name text) AS $$
+
+BEGIN
+  RETURN QUERY
+  SELECT i.item_id, i.image_url, i.name
+  FROM public.outfit_items oi
+  JOIN public.items i ON oi.item_id = i.item_id
+  WHERE oi.outfit_id = outfit_id;
+END;
+$$ LANGUAGE plpgsql;
+
