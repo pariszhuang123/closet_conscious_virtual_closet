92ca97fee74ecbc64882891259f99d883086f331
pariszhuang <pariszhuang@gmail.com>
2024-08-24 10:59:43 +1200

    To create new fetch function to check_nps_trigger

--- supabase/migrations/20240823220154_check_nps_trigger.sql
+++ supabase/migrations/20240823220154_check_nps_trigger.sql
@@ -20,7 +20,7 @@ BEGIN
     SELECT
         COALESCE(current_milestone, 0) AS outfits_created,
         CASE
-            WHEN current_milestone IN (30, 90, 150, 210, ...)  -- List your milestones here
+            WHEN current_milestone IN (30, 90, 150, 210, 270, 330. 390, 450, 510, 570, 630, 690, 750, 810, 870, 930, 990, 1050, 1110, 1170, 1230, 1290, 1350, 1410, 1470. 1530, 1590, 1650, 1710, 1770, 1830)  -- List your milestones here
                  AND (reviews.milestone_triggered IS NULL OR reviews.milestone_triggered <> current_milestone)
             THEN TRUE  -- Milestone reached, but not yet triggered, show NPS
             ELSE FALSE -- Milestone not reached or already triggered, do not show NPS
