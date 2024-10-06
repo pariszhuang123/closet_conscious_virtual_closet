61c86b16a9ff29c557b01c6807e7ead89d7b9b3c
pariszhuang <pariszhuang@gmail.com>
2024-10-06 13:53:36 +1300

    Features updated - Update the alter items table so that worn_in_outfit if not used, then equates to original price of the item

--- supabase/migrations/20241006004449_alter_items_table_price_per_wearv2.sql
+++ supabase/migrations/20241006004449_alter_items_table_price_per_wearv2.sql
@@ -1,8 +1,10 @@
 ALTER TABLE public.items
-ALTER COLUMN price_per_wear SET DATA TYPE NUMERIC GENERATED ALWAYS AS (
+DROP COLUMN price_per_wear;
+
+ALTER TABLE public.items
+ADD COLUMN price_per_wear NUMERIC GENERATED ALWAYS AS (
   CASE
     WHEN worn_in_outfit = 0 THEN amount_spent
     ELSE amount_spent / worn_in_outfit
   END
 ) STORED;
-
