12984c16b9efbebf546f99048fd5312ba0f30b3e
pariszhuang <pariszhuang@gmail.com>
2025-02-15 10:30:08 +1300

    Remove the package for image_resizer as in the future I am using a 3rd party app for background removal. There is no need to do compression via another library.

--- lib/core/user_photo/usecase/photo_capture_service.dart
+++ lib/core/user_photo/usecase/photo_capture_service.dart
@@ -1,25 +1,22 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import '../../utilities/image_resizer.dart';
 
 class PhotoCaptureService {
   final ImagePicker picker = ImagePicker();
 
-  // Capture a photo at max width 1024 and quality 100, then process further
+  // Capture a photo with compression directly in image_picker
   Future<File?> captureAndResizePhoto() async {
     final XFile? image = await picker.pickImage(
       source: ImageSource.camera,
       maxWidth: 1024,  // Limit image width
-      imageQuality: 100, // No compression yet (max quality)
+      imageQuality: 85, // Apply compression directly in image_picker
     );
 
     if (image != null) {
-      // Send captured image to ImageResizer for final compression
-      File resizedImageFile = await ImageResizer.compressToJpeg(File(image.path));
-      return resizedImageFile;
+      return File(image.path); // Directly return compressed image
     } else {
-      return null;  // Return null if user cancels
+      return null; // Return null if user cancels
     }
   }
 }
